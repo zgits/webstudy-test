2acd90224b70ad6296c7457164c1b47821086af7
zhang.junfei <43234936+zgits@users.noreply.github.com>
2020-03-09 18:58:19 +0800

    添加课程章节效果完成

--- src/views/webstudy-admin/addCourse.vue
+++ src/views/webstudy-admin/addCourse.vue
@@ -1,150 +1,289 @@
 <template>
-    <div class="components-container">
-      <el-steps :active="active" process-status="finish" finish-status="success">
-        <el-step title="课程信息"></el-step>
-        <el-step title="详细信息"></el-step>
-        <el-step title="步骤 3"></el-step>
-      </el-steps>
+  <div class="components-container">
+    <el-steps :active="active" process-status="finish" finish-status="success">
+      <el-step title="课程信息"></el-step>
+      <el-step title="详细信息"></el-step>
+      <el-step title="步骤 3"></el-step>
+    </el-steps>
 
 
+    <el-form v-if="active===0" ref="editFormRef" :model="editForm" label-width="70px" align="left">
 
+      <el-form-item label="课程名称">
+        <el-input v-model="editForm.className" style="width: 300px"/>
+      </el-form-item>
+      <el-form-item label="难度">
+        <el-radio-group v-model="editForm.level">
+          <el-radio :label="1">入门级</el-radio>
+          <el-radio :label="2">初级</el-radio>
+          <el-radio :label="3">中级</el-radio>
+          <el-radio :label="4">高级</el-radio>
+        </el-radio-group>
+      </el-form-item>
 
-        <el-form v-if="active===0" ref="editFormRef" :model="editForm" label-width="70px" align="left" >
-
-          <el-form-item label="课程名称">
-            <el-input v-model="editForm.className" style="width: 300px" />
-          </el-form-item>
-          <el-form-item label="难度">
-            <el-radio-group v-model="editForm.level">
-              <el-radio :label="1">入门级</el-radio>
-              <el-radio :label="2">初级</el-radio>
-              <el-radio :label="3">中级</el-radio>
-              <el-radio :label="4">高级</el-radio>
-            </el-radio-group>
-          </el-form-item>
-
-          <el-form-item label="简介">
-            <el-input v-model="editForm.introdu" type="textarea" />
-          </el-form-item>
-          <el-form-item label="封面">
-            <el-upload
-              limit="1"
-              action="#"
-              list-type="picture-card"
-              :auto-upload="false"
+      <el-form-item label="简介">
+        <el-input v-model="editForm.introdu" type="textarea"/>
+      </el-form-item>
+      <el-form-item label="封面">
+        <el-upload
+          limit="1"
+          action="#"
+          list-type="picture-card"
+          :auto-upload="false"
+        >
+          <i slot="default" class="el-icon-plus"/>
+          <div slot="file" slot-scope="{file}">
+            <img
+              class="el-upload-list__item-thumbnail"
+              :src="file.url"
+              alt=""
             >
-              <i slot="default" class="el-icon-plus" />
-              <div slot="file" slot-scope="{file}">
-                <img
-                  class="el-upload-list__item-thumbnail"
-                  :src="file.url"
-                  alt=""
-                >
-                <span class="el-upload-list__item-actions">
+            <span class="el-upload-list__item-actions">
                 <span
                   class="el-upload-list__item-preview"
                   @click="handlePictureCardPreview(file)"
                 >
-                  <i class="el-icon-zoom-in" />
+                  <i class="el-icon-zoom-in"/>
                 </span>
                 <span
                   v-if="!disabled"
                   class="el-upload-list__item-delete"
                   @click="handleDownload(file)"
                 >
-                  <i class="el-icon-download" />
+                  <i class="el-icon-download"/>
                 </span>
                 <span
                   v-if="!disabled"
                   class="el-upload-list__item-delete"
                   @click="handleRemove(file)"
                 >
-                  <i class="el-icon-delete" />
+                  <i class="el-icon-delete"/>
                 </span>
               </span>
+          </div>
+        </el-upload>
+        <el-dialog :visible.sync="dialogVisible">
+          <img width="100%" :src="dialogImageUrl" alt="">
+        </el-dialog>
+      </el-form-item>
+    </el-form>
+
+
+    <el-row v-if="active===1" :gutter="20" style="margin-top:50px;">
+      <el-col :span="8">
+        <el-card class="box-card" style="overflow: auto">
+          <div slot="header" class="clearfix">
+            <span>新建章节</span>
+          </div>
+          <div class="component-item" style="height:420px;">
+            <el-button style="width: 100%;" size="medium" type="primary" @click="add">新增章节</el-button>
+
+            <!--              <div  v-for="(d,index) in counter" :key="index">-->
+            <!--                <div class="board-item">-->
+            <!--                  <i  class="el-icon-plus" @click="addChapter"></i>-->
+            <!--                  第{{index+1}}章：标题-->
+
+            <!--                  </div>-->
+            <!--                <div v-for="(d,indexChild) in children" :key="indexChild">-->
+            <!--                  {{index+1}}-{{indexChild+1}}: 子标题-->
+            <!--                </div>-->
+
+            <!--              </div>-->
+
+
+            <div class="custom-tree-container">
+              <div class="block">
+                <el-tree
+                  :data="data"
+                  node-key="id"
+                  default-expand-all
+                  :expand-on-click-node="false"
+                  @node-click="clickTree">
+                  <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span v-if="data.children">
+          <el-button
+            type="text"
+            size="mini"
+            icon="el-icon-plus"
+            @click="() => append(data)">
+            新增
+          </el-button>
+          <el-button
+            type="text"
+            size="mini"
+            icon="el-icon-delete"
+            @click="() => remove(node, data)">
+            删除
+          </el-button>
+        </span>
+      </span>
+                </el-tree>
               </div>
-            </el-upload>
-            <el-dialog :visible.sync="dialogVisible">
-              <img width="100%" :src="dialogImageUrl" alt="">
-            </el-dialog>
-          </el-form-item>
-        </el-form>
-
-
-      <el-row v-if="active===1" :gutter="20" style="margin-top:50px;">
-        <el-col :span="8">
-          <el-card class="box-card">
-            <div slot="header" class="clearfix">
-              <span>新建章节</span>
             </div>
-            <div class="component-item" style="height:420px;">
-              <el-button @click="add">新增章节</el-button>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>编辑章节</span>
+          </div>
+          <div class="component-item" style="height:420px;">
+
+            <el-input @input="changeValue" v-model="input" placeholder='请输入内容' ></el-input>
 
 
-              <div v-for="(d,index) in counter" :key="index">
-                <div>我是一个组件</div>
 
-              </div>
-            </div>
-          </el-card>
-        </el-col>
-        <el-col :span="8">
-          <el-card class="box-card">
-            <div slot="header" class="clearfix">
-              <span>编辑章节</span>
-            </div>
-            <div class="component-item" style="height:420px;">
-              <dropdown-menu :items="articleList" style="margin:0 auto;" title="系列文章" />
-            </div>
-          </el-card>
-        </el-col>
-        <el-col :span="8">
-          <el-card class="box-card">
-            <div slot="header" class="clearfix">
-              <span>Share</span>
-            </div>
-            <div class="component-item" style="height:420px;">
-              <dropdown-menu :items="articleList" style="margin:0 auto;" title="系列文章" />
-            </div>
-          </el-card>
-        </el-col>
-      </el-row>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="8">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>Share</span>
+          </div>
+          <div class="component-item" style="height:420px;">
+            <dropdown-menu :items="articleList" style="margin:0 auto;" title="系列文章"/>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
 
 
-      <el-button style="margin-top: 12px;" @click="next">下一步</el-button>
+    <el-button style="margin-top: 12px;" @click="next">下一步</el-button>
 
 
-    </div>
+  </div>
 </template>
 
 <script>
+  let id = 0;
+  let count=1000;
+  export default {
+    name: "addCourse",
+    data() {
+      return {
+        active: 0,
+        editForm: {
+          level: '',
+          className: ''
 
-    export default {
-        name: "addCourse",
-      data() {
-        return {
-          active: 0,
-          counter:[],
-          editForm: {
-            level: '',
-            className: ''
+          // mobile: ''
+        },
+        data:[],
+        input:'',
+        inputData: '',
+        suffix:'',
+      };
+    },
 
-            // mobile: ''
-          },
-        };
+    methods: {
+      next() {
+        if (this.active++ > 2) this.active = 0;
+      },
+      add() {
+
+        id++;
+        this.data.push({
+          id:id,
+          label:'第'+(id)+'章:标题',
+          children:[],
+        });
       },
 
-      methods: {
-        next() {
-          if (this.active++ > 2) this.active = 0;
-        },
-        add(){
-          this.counter.push({});
+      append(data) {
+        count++;
+        const newChild = { id: count, label: data.id+'-'+(data.children.length+1)+':子标题' };
+        data.children.push(newChild);
+      },
+
+      remove(node, data) {
+        id--;
+        const parent = node.parent;
+        const children = parent.data.children || parent.data;
+        const index = children.findIndex(d => d.id === data.id);
+        children.splice(index, 1);
+
+        this.data.forEach(item=>{
+
+          if(item.id>data.id) {
+
+
+            var tempId = --item.id;
+            item.id = tempId;
+            item.label = '第' + (tempId) + '章:' + item.label.split(":")[1];
+
+            item.children.forEach(child => {
+              child.label = tempId + "-" + child.label.split("-")[1];
+            })
+
+          }
+
+
+        })
+
+      },
+
+      clickTree(data){
+        this.suffix=data.label.split(":")[0];
+        this.input=data.label.split(":")[1];
+        this.inputData=data.id;
+        console.log(data);
+      },
+
+      changeValue(){
+
+        if (this.inputData>1000){
+            this.data.forEach(item=>{
+              item.children.forEach(child=>{
+                if(child.id==this.inputData){
+                  child.label=this.suffix+':'+this.input;
+                }
+              });
+            });
+        } else{
+          this.data[this.inputData-1].label=this.suffix+':'+this.input;
         }
       }
+
+      // renderContent(h, { node, data, store }) {
+      //   return (
+      //     <span class="custom-tree-node">
+      //     <span>{node.label}</span>
+      //     <span>
+      //     <el-button size="mini" type="text" on-click={ () => this.append(data) }>Append</el-button>
+      //   <el-button size="mini" type="text" on-click={ () => this.remove(node, data) }>Delete</el-button>
+      //   </span>
+      //   </span>);
+      // }
     }
+  }
 </script>
 
 <style scoped>
+
+
+  .custom-tree-node {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
+    padding-right: 8px;
+  }
+
+  .board-item {
+    cursor: pointer;
+    width: 100%;
+    height: 64px;
+    margin: 5px 0;
+    background-color: #fff;
+    text-align: left;
+    line-height: 54px;
+    padding: 5px 10px;
+    box-sizing: border-box;
+    box-shadow: 0px 1px 3px 0 rgba(0, 0, 0, 0.2);
+  }
 
 </style>
